65c2b5c1a2d8aff9d908e7ebd35d07d28ee29920
Emre Savaş <emre_savas28@hotmail.com>
2023-10-24 15:20:34 +0300

    Define 'ConfigureSqlContext' as a static function and use it in 'program.cs'.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Repositories.EFCore;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +11,7 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<RepositoryContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"))
-);
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 var app = builder.Build();
 
