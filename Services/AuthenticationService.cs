ce5df7d89e9a13aa0a7dfe59f7fa336559ebe8bd
Emre Savaş <emre_savas28@hotmail.com>
2023-10-29 16:36:55 +0300

    Implement ValidateUser function.

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -7,6 +7,7 @@ using Services.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,8 @@ namespace Services
         private readonly IMapper _mapper;
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
+
+        private User? _user;
 
         public AuthenticationService(ILoggerService logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
         {
@@ -36,6 +39,17 @@ namespace Services
             if (result.Succeeded)
             {
                 await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            }
+            return result;
+        }
+
+        public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuthenticationDto)
+        {
+            _user = await _userManager.FindByNameAsync(userForAuthenticationDto.UserName);
+            var result = (_user != null && await _userManager.CheckPasswordAsync(_user, userForAuthenticationDto.Password));
+            if(!result)
+            {
+                _logger.LogWarning($"{nameof(ValidateUser)}: Authentication failed. Wrong user name or password.");
             }
             return result;
         }
--- Services/Contracts/IAuthenticationService.cs
+++ Services/Contracts/IAuthenticationService.cs
@@ -11,5 +11,6 @@ namespace Services.Contracts
     public interface IAuthenticationService
     {
         Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuthenticationDto);
     }
 }
